92ed7b8a4a7eff2dd92768d9e924f2479d590a84
jfield360 <83071699+jfield360@users.noreply.github.com>
2021-05-12 10:03:58 +0100

    Add files via upload

---
+++ 2-3 Adding Numerical Strings.py
@@ -0,0 +1,6 @@
+print("Stones sir?")
+pointyStones = input("Number of pointy ones: ")
+flatStones = input("Number of flat ones: ")
+gravel = input("Packets of gravel: ")
+total = pointyStones + flatStones + gravel
+print("Your total is:", total)
